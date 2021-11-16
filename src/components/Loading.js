54bfda90d6e8b123965bd1ae9b61245222ff0b97
Ben Marshall <benleem98@yahoo.com>
2021-11-15 22:11:32 -0500

    added loading animation

---
+++ src/components/Loading.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { css } from "@emotion/react";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+`;
+
+const Loading = () => {
+    return (
+        <div className='loading'>
+            <ClipLoader loading={true} color='#dd7230' size={200} css={override}/>
+        </div>
+    )
+}
+
+export default Loading
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Post from './Post'
+import Loading from './Loading'
 
 const Posts = ({fetchData, posts, loading}) => {
     return (
         <div className='posts'>
-            {loading ? "Loading..." : posts.map(post => <Post fetchData={fetchData} key={post.ref["@ref"].id} post={post}/>).reverse()}
+            {loading ? <Loading/> : posts.map(post => <Post fetchData={fetchData} key={post.ref["@ref"].id} post={post}/>).reverse()}
         </div>
     )
 }
